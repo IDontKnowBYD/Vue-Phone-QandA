b6e6d402510d96ad483a00c23fdaf03f9235957d
zhushaofeng <zhushaofeng19960409@gmail.com>
2017-12-20 10:32:47 +0800

    add qanda

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../page/home')
+    },
+    {
+      path: '/item',
+      name: 'item',
+      component: () => import('../page/item')
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: () => import('../page/score')
+    }
+  ]
+})
