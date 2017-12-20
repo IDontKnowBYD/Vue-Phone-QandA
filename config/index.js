719f2922f99d3a925dec27533df0b5ec5bc55ef7
zhushaofeng <zhushaofeng19960409@gmail.com>
2017-12-20 15:23:17 +0800

    change

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/VUE-QANDA／',
+    assetsPublicPath: '/VUE-QandA／',
 
     /**
      * Source Maps
